5daf543ebebadddd1cb6d9bc34bd99c5a7eaabc7
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-18 08:33:58 -0300

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import { router } from "./routes/routes";
+import cors from "cors";
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/", router);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${  port}`);
 });
